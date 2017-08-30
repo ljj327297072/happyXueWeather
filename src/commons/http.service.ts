2fc1e6fe0ef1a56b22fab44cdafc7365d2939a9f
刘俊杰 <ljj327297072@163.com>
2017-08-30 22:42:54 +0800

    httpservice

---
+++ src/commons/http.service.ts
@@ -0,0 +1,38 @@
+/**
+ * Created by ljj32 on 2017/8/30.
+ */
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class HttpService {
+  host: string = "https://free-api.heweather.com/v5";
+  key: string = "788a850cd30749189eb546dbdd2107bb";
+  constructor(public http: Http) {
+
+  }
+  /*
+  * get请求
+  * */
+  get(url: string, params: URLSearchParams): Promise<JSON> {
+    params.set("key", this.key);
+    console.log(this.genUrl(url));
+    console.log(params);
+    return new Promise((resolve, reject) => {
+      this.http.get(this.genUrl(url), {search: params})
+        .map(res => res.json())
+        .subscribe(data => {
+          resolve(data);
+        }, err => {
+          reject
+        });
+    })
+  }
+  /*
+   * 处理请求链接
+   * */
+  genUrl(url): string{
+    return this.host + url;
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpService} from "../../commons/http.service";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+                public http: HttpService
+  ) {
+    let params = new URLSearchParams();
+    params.set("city", "青岛");
+    this.http.get("/weather", params).then(res => {
+      console.log(res);
+    })
   }
 
 }
